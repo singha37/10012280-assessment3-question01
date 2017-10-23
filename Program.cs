10ebb8244d9aba36034282ed454cbce5914974c3
Amandeep Singh <10012280@BCS.NET.NZ>
2017-10-24 11:38:23 +1300

    initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Random_Number
+{
+    class Program
+    {
+        static List<string> Falconers = new List<string> { "Debbie","Heidi","Noel","Melissa","Kirsten","Koshy" };
+        static List<string> Falcons = new List<string> { "Hisan","Ka Kite","Siren","Koa","Stella","Mojo" };
+        static void Main(string[] args)
+        {
+                 //Start the program with Clear();
+            Console.Clear();
+
+            Console.WriteLine(DisplayFalcons());
+            Console.WriteLine(DisplayFalcons());
+            Console.WriteLine(DisplayFalcons());
+
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+             static int GenerateRandomNumber(List<string> myList)
+        {
+            var rand = new Random();
+            return rand.Next(1, myList.Count);
+        }
+
+        static string DisplayFalcons()
+        {
+
+            var selectedFalconers = Falconers[GenerateRandomNumber(Falconers)];
+            var selectedFalcons = Falcons[GenerateRandomNumber(Falcons)];
+
+            
+            return $"Selected Falcons = {selectedFalcons}\nSelected Falconers = {selectedFalconers}";
+        }
+    } 
+}
